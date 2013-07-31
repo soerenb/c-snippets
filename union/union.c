2707b3dd72a65bedbd76bd4b5c8487e35c960d79
Nick Ma <nick.mayerhofer@hotmail.com>
2013-07-31 12:21:00 +0200

    union: Added another snippet
    'union bar' shows how to access bits of a structure. That is useful
    within low level programming, or serializing/deserializing

--- union/union.c
+++ union/union.c
@@ -15,6 +15,28 @@
 #include <stdio.h>
 #include <stdint.h>
 
+/* gives us the ability to access single bits in byte1 */
+struct byte1_bitfields {
+	uint8_t bit0 :1; /* Least siginificant bit */
+	uint8_t bit1 :1;
+	uint8_t bit2 :1;
+	uint8_t bit3 :1;
+	uint8_t bit4_7 :4; /* Most significant bits */
+};
+
+union bar {
+	uint8_t allElements[4];
+
+	struct {
+		uint8_t byte0;
+		union {
+			uint8_t byte1;
+			struct byte1_bitfields byte1_elements;
+		};
+		uint8_t bytes[2];
+	} elements;
+} __attribute__((packed));
+
 union foo {
 	uint32_t int32;
 	uint16_t int16;
@@ -23,6 +45,7 @@ union foo {
 
 int main(int argc, char **argv)
 {
+	/* Foo tests */
 	union foo my_union;
 
 	my_union.int32 = 0xdeadbeef;
@@ -30,6 +53,21 @@ int main(int argc, char **argv)
 	printf("int32: %#x\n", my_union.int32);
 	printf("int16: %#x\n", my_union.int16);
 	printf("int8 : %#x\n", my_union.int8);
+
+	/* Bar tests */
+	/* Accessing serialized bytes and/or bits */
+	union bar my_bar;
+
+	my_bar.allElements[0] = 0xAA;
+	printf("byte0:\t %#02x\n", my_bar.elements.byte0);
+
+	my_bar.elements.byte1 = 0x2E; /* equals 0b0010 1110 */
+	printf("byte1-bit0:\t %#02x\n", my_bar.elements.byte1_elements.bit0);
+	printf("byte1-bit1:\t %#02x\n", my_bar.elements.byte1_elements.bit1);
+	printf("byte1-bit2:\t %#02x\n", my_bar.elements.byte1_elements.bit2);
+	printf("byte1-bit3:\t %#02x\n", my_bar.elements.byte1_elements.bit3);
+	printf("byte1-bits[4..7]: %#02x\n\n",
+			my_bar.elements.byte1_elements.bit4_7);
 
 	return 0;
 }
